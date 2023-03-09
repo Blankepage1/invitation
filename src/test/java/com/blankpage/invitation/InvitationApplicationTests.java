905659acf49fb623e6328624699fab55002a95ea
nana <dheerazpandey@gmail.com>
2023-03-09 15:11:22 -0500

    Initial commit

---
+++ src/test/java/com/blankpage/invitation/InvitationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blankpage.invitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvitationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
