ae20095abdb14c14b6fa4cae37eed5dac2b734ef
nana <dheerazpandey@gmail.com>
2023-03-10 00:37:17 -0500

    config server included

--- src/main/java/com/blankpage/invitation/TestController.java
+++ src/main/java/com/blankpage/invitation/TestController.java
@@ -1,13 +1,18 @@
 package com.blankpage.invitation;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${server.retry}")
+    String name = "World";
+
     @GetMapping("/")
-    public String hello(){
-        System.out.println("Hello");
+    public String hello() {
+        System.out.println("Hello " + name);
         return "hello";
     }
 }
